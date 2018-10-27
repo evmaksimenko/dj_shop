e3e1b37c0b3ea28e2b31d2cb3b67688a7768bca2
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2018-10-27 10:26:51 +0700

    Change item detailed view from function to class-based. Modify url pattern.

--- djadesh/urls.py
+++ djadesh/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'djadesh'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:item_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.ItemView.as_view(), name='detail'),
 ]
--- djadesh/views.py
+++ djadesh/views.py
@@ -1,13 +1,13 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views import generic
 from .models import Item
 
 
-class IndexView(ListView):
+class IndexView(generic.ListView):
     model = Item
     template_name = 'djadesh/index.html'
 
 
-def detail(request, item_id):
-    return HttpResponse("Hello, world. You're at the details. #{}".format(item_id))
+class ItemView(generic.DetailView):
+    model = Item
+    template_name = 'djadesh/item.html'
