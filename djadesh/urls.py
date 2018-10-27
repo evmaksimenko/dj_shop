b631300da80e063c7485b220f709d904860a2fe4
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2018-10-27 22:32:51 +0700

    Fix media path pattern

--- djadesh/urls.py
+++ djadesh/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.ItemView.as_view(), name='detail'),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
