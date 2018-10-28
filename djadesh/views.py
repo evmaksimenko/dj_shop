887ba902fc95901f6449501d5721538497c3784b
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2018-10-28 22:38:24 +0700

    Modify views.py.
    Add imagekit processors for Previews and Thumbnails.

--- djadesh/views.py
+++ djadesh/views.py
@@ -1,5 +1,7 @@
-# from django.shortcuts import render
 from django.views import generic
+from imagekit import ImageSpec, register
+from imagekit.processors import ResizeToFill, ResizeToFit, SmartResize
+
 from .models import Item
 
 
@@ -11,3 +13,19 @@ class IndexView(generic.ListView):
 class ItemView(generic.DetailView):
     model = Item
     template_name = 'djadesh/item.html'
+
+
+class Preview(ImageSpec):
+    processors = [ResizeToFit(300, 300, False)]
+    format = 'JPEG'
+    options = {'quality': 90}
+
+
+class Thumbnail(ImageSpec):
+    processors = [ResizeToFill(50, 50)]
+    format = 'JPEG'
+    options = {'quality': 60}
+
+
+register.generator('djadesh:preview', Preview)
+register.generator('djadesh:thumbnail', Thumbnail)
