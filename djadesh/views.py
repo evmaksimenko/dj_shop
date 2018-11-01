79258c8038a29eeb612f4c0abe430ddfa8870709
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2018-11-01 23:45:25 +0700

    Add basket item to the index page. Modify imagekit templates settings.

--- djadesh/views.py
+++ djadesh/views.py
@@ -1,6 +1,6 @@
 from django.views import generic
 from imagekit import ImageSpec, register
-from imagekit.processors import ResizeToFill, ResizeToFit, SmartResize
+from imagekit.processors import ResizeToFill, ResizeToFit
 
 from .models import Item
 
@@ -8,6 +8,16 @@ from .models import Item
 class IndexView(generic.ListView):
     model = Item
     template_name = 'djadesh/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if not self.request.session.get('basket_items', []):
+            self.request.session['basket_items'] = []
+            self.request.session['basket_items_count'] = 0
+            self.request.session['basket_total_price'] = 0
+        context['basket_items_count'] = self.request.session.get('basket_items_count', 0)
+        context['basket_total_price'] = self.request.session.get('basket_total_price', 0)
+        return context
 
 
 class ItemView(generic.DetailView):
@@ -21,11 +31,11 @@ class Preview(ImageSpec):
     options = {'quality': 90}
 
 
-class Thumbnail(ImageSpec):
-    processors = [ResizeToFill(50, 50)]
+class MainImage(ImageSpec):
+    processors = [ResizeToFit(500, 500, False)]
     format = 'JPEG'
-    options = {'quality': 60}
+    options = {'quality': 90}
 
 
 register.generator('djadesh:preview', Preview)
-register.generator('djadesh:thumbnail', Thumbnail)
+register.generator('djadesh:mainimage', MainImage)
