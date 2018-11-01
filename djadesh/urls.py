a8fcf7ffc4ac2852959ee1039bb334184ce3c0f8
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2018-11-01 23:43:38 +0700

    Modify MEDIA path settings.

--- djadesh/urls.py
+++ djadesh/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 
 from . import views
 
@@ -9,6 +7,3 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.ItemView.as_view(), name='detail'),
 ]
-
-if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
