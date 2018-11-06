a19f86e1c7d44f6cfac5c2cc532f3680da25203d
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2018-11-06 22:22:31 +0700

    urls.py. Add links to basket and item add page.

--- djadesh/urls.py
+++ djadesh/urls.py
@@ -6,4 +6,6 @@ app_name = 'djadesh'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.ItemView.as_view(), name='detail'),
+    path('<int:item_id>/add/', views.basket_add, name='add'),
+    path('basket/', views.basket, name='basket'),
 ]
