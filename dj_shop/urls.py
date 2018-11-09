72506f7b298e61be32a4ec3ff8631a1051cd7d59
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2018-11-09 22:53:44 +0700

    Set error 404 and 500 handlers.

--- dj_shop/urls.py
+++ dj_shop/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.utils.functional import curry
+from django.views.defaults import *
+
 urlpatterns = [
     path('shop/', include('djadesh.urls')),
     path('admin/', admin.site.urls),
@@ -10,3 +13,6 @@ urlpatterns = [
 
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler500 = curry(server_error, template_name='500.html')
+handler404 = curry(page_not_found, template_name='404.html')
